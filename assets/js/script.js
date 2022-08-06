a8c3c33b2245a2b873253ae1e1a0ef0773acbfdf
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-08-05 22:45:44 -0600

    added button to html and evenlistener for button in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
 // array of objects containing words and blanks:
 var game = [
-    js = {
-        word: "javascript",
-        blank: ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_"]
-    },
+  (js = {
+    word: "javascript",
+    blank: ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+  }),
 
-    func = {
-        word: "function",
-        blank: ["_", "_", "_", "_", "_", "_", "_", "_"]
-    },
+  (func = {
+    word: "function",
+    blank: ["_", "_", "_", "_", "_", "_", "_", "_"],
+  }),
 
-    variable = {
-        word: "variable",
-        blank: ["_", "_", "_", "_", "_", "_", "_", "_"]
-    },
+  (variable = {
+    word: "variable",
+    blank: ["_", "_", "_", "_", "_", "_", "_", "_"],
+  }),
 
-    object = {
-        word: "object",
-        blank: ["_", "_", "_", "_", "_", "_"]
-    },
+  (object = {
+    word: "object",
+    blank: ["_", "_", "_", "_", "_", "_"],
+  }),
 
-     array = {
-        word: "array",
-        blank: ["_", "_", "_", "_", "_"]
-     }
+  (array = {
+    word: "array",
+    blank: ["_", "_", "_", "_", "_"],
+  }),
 ];
 
 // math function to select random array index:
@@ -33,14 +33,19 @@ var randomWord = game[random];
 // variable to store underscore blanks:
 var guessBlank = randomWord.blank;
 
-// variables to store dynamic creation of html:
+// variables to store dynamic creation of html and eventlisteners:
+var click = document.querySelector(".button");
 var keyPress = document.querySelector("body");
 var div = document.createElement("div");
 
 // =========================================================
 
+// eventlistener begins game on start button:
+click.addEventListener("click", function () {
+  render();
+});
+
 // dynamically renders blanks to screen:
-render();
 
 function render() {
   div.innerHTML = randomWord.blank.join(" ");
@@ -50,22 +55,22 @@ function render() {
 
 // upon keydown, user guess is evaluated against array:
 keyPress.addEventListener("keydown", function (event) {
-    var key = event.key.toLowerCase();
-    // splits word from object in game array above into an array
-    var guessWord = randomWord.word.split("");
-  
-    // loop through array containing randomly selected word
-    for (i = 0; i < guessWord.length; i++) {
-        // if keypress = index of array
-      if (key === guessWord[i]) {
-        guessBlank[i] = key;
-        renderAnswer();
-      }
+  var key = event.key.toLowerCase();
+  // splits word from object in game array above into an array
+  var guessWord = randomWord.word.split("");
+
+  // loop through array containing randomly selected word
+  for (i = 0; i < guessWord.length; i++) {
+    // if keypress = index of array
+    if (key === guessWord[i]) {
+      guessBlank[i] = key;
+      renderAnswer();
     }
-  });
-  
+  }
+});
+
 //   dynamically renders user guess to html:
-  function renderAnswer() {
-    div.innerHTML = guessBlank.join(" ");
-    document.body.appendChild(div);
-  };
+function renderAnswer() {
+  div.innerHTML = guessBlank.join(" ");
+  document.body.appendChild(div);
+}
